cbf011b89d51a4131231c0a16d74c33a33504360
Zach Kellen <z.kellen12@gmail.com>
2021-08-20 10:40:35 -0500

    palindrome update

---
+++ day2.js
@@ -0,0 +1,119 @@
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+// a stack! last in, first out
+// we add/remove from the top of a stack
+// what's going to be the "top" of our stack here?
+
+class SLLStack {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // push(value) - adds the given value to the stack
+    push(value) {
+        var newNode = new ListNode(value)
+        if(this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+    
+    pop() {
+        if(this.head == null && this.tail == null) {
+            return undefined
+        }
+        else if (this.head == this.tail) {
+            var temp = this.head;
+            this.head = null;
+            this.tail = null;
+            return temp.value;
+        }
+        else {
+            var temp = this.head.next;
+            var oldHead = this.head;
+            this.head.next = null;
+            this.head = temp;
+        }
+        return oldHead.value;
+    }
+
+    // top() - returns the top value without removing it
+    top() {
+        if(this.head == null && this.tail == null) {
+            return undefined
+        }
+        else {
+            return this.head.value;
+        }
+    }
+
+    // contains(target) - returns true if the target value is in the stack,
+    // false if not
+    contains(target) {
+        if(this.head == null && this.tail == null) {
+            return false;
+        }
+        var runner = this.head;
+        while(runner != null) {
+            if(runner.value == target) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    // isEmpty() - returns true if the stack is empty, false otherwise
+    isEmpty() {
+        // if(this.head == null && this.tail == null) {
+        //     return true;
+        // }
+        // return false;
+        return (this.head == null && this.tail == null);
+    }
+
+
+    size() {
+        if(this.head == null && this.tail == null) {
+            return 0;
+        }
+        var runner = this.head;
+        var counter = 0;
+        while(runner != null) {
+            counter++;
+            runner = runner.next;
+        }
+        return counter;
+    }
+    
+}
+
+var x = new SLLStack();
+
+console.log(x.pop());
+console.log(x.top());
+console.log(x.contains(1));
+console.log(x.isEmpty());
+console.log(x.size());
+
+x.push(7);
+x.push(3);
+x.push(8);
+
+console.log(x.top());
+console.log(x.pop());
+console.log(x.contains(8));
+console.log(x.contains(7));
+console.log(x.isEmpty());
+console.log(x.size());
+
--- queues.js
+++ queues.js
@@ -89,13 +89,167 @@ class SLLQueue {
         return true;
     }
 
+    // isPalindrome() - return true if the values of the queue form a palindrome,
+    // and false otherwise. don't put the values of the queue into an array!
+    // (or turn them into a string, either - your queue listnode values
+    // may not always be able to be turned into a string)
+    // do not modify the queue state in any way
+    // also don't add some kind of tricky extra queue methods - they're not needed
+    // maybe... use a stack? you'll need to copy that class into this file
+    // you ain't gotta tho nbd it's just a suggestion
+
+    // isPalindrome() {
+    // // Check head and tail value first
+    // if (this.head.value != this.tail.value) {
+    //     return false;
+    // }
+    // var runner = this.head;
+    // var runner2 = this.head
+    // var marker = this.tail;
+    // while(runner.value != null) {
+    //     if (runner.value != marker.value) {
+    //         return false
+    //     }
+    //     runner = runner.next
+    //     console.log("runner =" , runner)
+    //     console.log("runner2 =" , runner2)
+    //     console.log("marker =" , marker)
+    //     while(runner2.next != marker) {
+    //         runner2 = runner2.next
+    //     }
+    //     marker = runner2
+    // }
+    // return true;
+    // }
+
+    isPalindrome() {
+        if (this.head == null) {
+            return false;
+        }
+        if (this.head.value != this.tail.value) {
+            return false;
+        }
+        
+        var runner = this.head;
+        var stack = new SLLStack()
+        while(runner != null) {
+            stack.push(runner.value);
+            runner = runner.next;
+        }
+        var runner2 = this.head;
+        while(runner2 != null) {
+            // console.log(runner2.value)
+            // console.log(stack.head.value)
+            // console.log("")
+            if(runner2.value != stack.head.value) {
+                return false;
+            }
+            runner2 = runner2.next;
+            stack.pop();
+        }
+        return true;
+    }
 }
 
-// compareQueues (even-numbered group)
-// return true if the queues have the same values in the same order
-// false otherwise
-// important: this is a non-destructive operation!
-// do not modify either queue
-// function compareQueues(queue1, queue2) {
+class SLLStack {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
 
-// }
+    // push(value) - adds the given value to the stack
+    push(value) {
+        var newNode = new ListNode(value)
+        if(this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
+    
+    pop() {
+        if(this.head == null && this.tail == null) {
+            return undefined
+        }
+        else if (this.head == this.tail) {
+            var temp = this.head;
+            this.head = null;
+            this.tail = null;
+            return temp.value;
+        }
+        else {
+            var temp = this.head.next;
+            var oldHead = this.head;
+            this.head.next = null;
+            this.head = temp;
+        }
+        return oldHead.value;
+    }
+
+    // top() - returns the top value without removing it
+    top() {
+        if(this.head == null && this.tail == null) {
+            return undefined
+        }
+        else {
+            return this.head.value;
+        }
+    }
+
+    // contains(target) - returns true if the target value is in the stack,
+    // false if not
+    contains(target) {
+        if(this.head == null && this.tail == null) {
+            return false;
+        }
+        var runner = this.head;
+        while(runner != null) {
+            if(runner.value == target) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    // isEmpty() - returns true if the stack is empty, false otherwise
+    isEmpty() {
+        // if(this.head == null && this.tail == null) {
+        //     return true;
+        // }
+        // return false;
+        return (this.head == null && this.tail == null);
+    }
+
+
+    size() {
+        if(this.head == null && this.tail == null) {
+            return 0;
+        }
+        var runner = this.head;
+        var counter = 0;
+        while(runner != null) {
+            counter++;
+            runner = runner.next;
+        }
+        return counter;
+    }
+    
+}
+
+var qA = new SLLQueue
+qA.enqueue('hi')
+qA.enqueue('hello')
+qA.enqueue('bye')
+qA.enqueue('hello')
+qA.enqueue('hi')
+// qA.enqueue(5)
+// qA.enqueue(4)
+// qA.enqueue(3)
+// qA.enqueue(2)
+// qA.enqueue(1)
+
+console.log(qA.isPalindrome())
